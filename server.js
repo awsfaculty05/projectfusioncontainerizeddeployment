32eb361a6accb22c4994e3d68c9ee802577ecc97
awsfaculty05 <awsfaculty05@fusion-institute.com>
2025-06-24 18:21:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This is fusion class and this is devops ');
+  res.send('MYDEVOPSCLASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSss ');
 });
 
 app.listen(PORT, HOST);
