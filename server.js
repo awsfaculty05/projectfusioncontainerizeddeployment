9affd9f43f927f3044f675816dd7f23e66ebd2bc
m2sclasses4ittech <m2sclasses4ittech@gmail.com>
2025-06-24 17:02:45 +0530

    modified server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOPS and AWS course completed by fusionw! ');
+  res.send('Devops1234444555555555555555555555555555555555555555555555555555555555 ');
 });
 
 app.listen(PORT, HOST);
