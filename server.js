5200dc0e3289218faddf47555865bc3342422f64
awsfaculty05 <awsfaculty05@fusion-institute.com>
2025-07-04 08:57:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('automatic update of nodejs website ');
+  res.send('welcome to Devops ');
 });
 
 app.listen(PORT, HOST);
